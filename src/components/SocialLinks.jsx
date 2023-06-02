b5826cbd323aee05e7a81a290a8f4e91f9976b20
holliday91 <j.holliday54@gmail.com>
2023-06-02 10:45:39 -0400

    resume_added1

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "resume2023jj.pdf",
+      href: "/resume2023jj.pdf",
       style: "rounded-br-md",
       download: true,
     },
